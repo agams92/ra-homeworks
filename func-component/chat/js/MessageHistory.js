edc5bc94a55f816181937473efde223de5421af5
Lev <agams92@gmail.com>
2018-07-11 21:14:52 +0300

    In which our hero fixed chat

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const MessageHistory = props => {
+    if(!Array.isArray(props.list) || props.list.length == 0) {
+        return null;
+    }
+    const listToRender = props.list.map(message => {
+        const CurrentMessage = message.type === 'response' ? Response : message.type === 'message' ? Message : Typing;
+        return <li key={message.id}><CurrentMessage from={message.from} message={message}/></li>
+    })
+    return(
+        <ul>
+            {listToRender}
+        </ul>
+    )
+}
