d06354e9cbb3d7ddcc428701e3f32b1274d7659a
Lev <agams92@gmail.com>
2018-07-11 16:28:21 +0300

    In which our hero fixed films and stars

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,20 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+  if(count < 1 || count > 5 || !isNumber(count)) {
+    return null;
+  }
+  const starsAmount = [];
+  for (let i = 0; i < count; i++) {
+    starsAmount.push(<li><Star /></li>);
+  }
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {starsAmount}
+    </ul>
+  );
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
