013ca09d8e84e317e5b2346145566b42e4b4c292
Lev <agams92@gmail.com>
2018-07-31 21:34:26 +0300

    In which our hero fixed searchBox

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -2,18 +2,35 @@ class SearchBox extends React.Component {
 
   constructor(props) {
     super(props);
+    this.isFixed = this.isFixed.bind(this);
+    this.setPosition = this.setPosition.bind(this);
     this.state = { fixed: false };
+  }
+
+  componentDidMount() {
+    this.searchBox = document.querySelector('.search-box');
+    this.isFixed()
+    window.addEventListener('scroll', this.setPosition);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.setPosition);
+  }
+
+  isFixed() {
+    if (this.searchBox.getBoundingClientRect().y <= 0) return true;
+    return false;
+  }
+
+  setPosition() {
+    if (this.isFixed()) {
+      this.setState({fixed: true})
+    } else {
+      this.setState({fixed: false})
+    }
   }
 
   render() {
     return <SearchBoxView fixed={this.state.fixed} />
-  }
-
-  isFixed() {
-    return undefined;
-  }
-
-  setPosition() {
-    return undefined;
   }
 }
