2d9c54b9eabcd0c67f46e1438fe98a50befa4d7b
Lev <agams92@gmail.com>
2018-08-01 23:08:37 +0300

    In which our hero fixed shop cart

--- lifecycle/lib/js/ProgressBar.js
+++ lifecycle/lib/js/ProgressBar.js
@@ -1,7 +1,29 @@
+
 class ProgressBar extends React.Component {
   render() {
     return (
-      <canvas id="progressCanvas" className="progress" />
+        <CircleChart />
     );
+  }
+}
+
+class CircleChart extends React.Component {
+  componentDidMount() {
+    const progessChart = new Chart (this.progessChart, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [100],
+          backgroundColor: 'red'
+        }]
+      },
+      options: {
+        cutoutPercentage: 50,
+        responsive: true
+      }
+    }) 
+  }
+  render () {
+    return <canvas id="progressCanvas" className="progress" ref={canvas => this.progessChart = canvas} />
   }
 }
