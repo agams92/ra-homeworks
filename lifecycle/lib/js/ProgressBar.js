72a98b2e834ac4c330e00fe0f745ff060005cd86
Lev <agams92@gmail.com>
2018-08-02 15:39:09 +0300

    In which our hero fixed progress bar

--- lifecycle/lib/js/ProgressBar.js
+++ lifecycle/lib/js/ProgressBar.js
@@ -1,28 +1,47 @@
 
 class ProgressBar extends React.Component {
-  render() {
-    return (
-        <CircleChart />
-    );
-  }
-}
 
-class CircleChart extends React.Component {
   componentDidMount() {
-    const progessChart = new Chart (this.progessChart, {
-      type: 'doughnut',
-      data: {
-        datasets: [{
-          data: [100],
-          backgroundColor: 'red'
-        }]
-      },
-      options: {
-        cutoutPercentage: 50,
-        responsive: true
-      }
-    }) 
+    this.progessChart.width = 200;
+    this.progessChart.height = 135;
+    this.renderCircles();
   }
+
+  componentDidUpdate() {
+    this.renderCircles();
+  }
+
+  renderCircles() {
+    const width = this.progessChart.width;
+    const height = this.progessChart.height;
+    const x = width / 2;
+    const y = height / 2;
+    const onePercent = 360 / 100;
+    const readedAmount = 100 * this.props.completed / this.props.total;
+    const result = onePercent * readedAmount;
+    const ctx = this.progessChart.getContext('2d');
+    for(let degrees = 0; degrees <= result; degrees++) {
+      ctx.clearRect(0,0,width,height);
+
+      ctx.beginPath();
+      ctx.arc(x,y,52,(Math.PI/180) * 270,(Math.PI/180) * (270 + 360) );
+      ctx.strokeStyle = '#4ca89a';
+      ctx.lineWidth = '7';
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(x,y,45,(Math.PI/180) * 270,(Math.PI/180) * (270 + degrees) );
+      ctx.strokeStyle = '#96d6f4';
+      ctx.lineWidth = '7';
+      ctx.stroke();
+
+      ctx.fillStyle = 'black';
+      ctx.font = '35px Helvetica';
+      ctx.textAlign = "center";
+      ctx.fillText(`${readedAmount.toFixed()}%`, x + 5, y + 10)
+    }
+  }
+
   render () {
     return <canvas id="progressCanvas" className="progress" ref={canvas => this.progessChart = canvas} />
   }
