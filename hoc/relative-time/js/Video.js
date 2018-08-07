fe586c862579cebe613af2acd945b27909183b31
Lev <agams92@gmail.com>
2018-08-07 13:21:59 +0300

    In which our hero beautified date

--- hoc/relative-time/js/Video.js
+++ hoc/relative-time/js/Video.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const PrettyDateTime = dateBeautifier(DateTime);
+
 const Video = props => {
     return (
         <div className="video">
             <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTime date={props.date} />
+            <PrettyDateTime date={props.date} />
         </div>
     )
 };
