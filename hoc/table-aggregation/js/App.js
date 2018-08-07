74994d9b751cf1dcdc9237b70b9f04ee7790703e
Lev <agams92@gmail.com>
2018-08-07 15:45:45 +0300

    In which our hero sorted tables

--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -17,10 +17,47 @@ class App extends React.Component {
     render() {
         return (
             <div id="app">
-                <MonthTable list={this.state.list} />
-                <YearTable list={this.state.list} />
-                <SortTable list={this.state.list} />
+                <SortedMonth list={this.state.list} />
+                <SortedYear list={this.state.list} />
+                <SortedTable list={this.state.list} />
             </div>
         );
     }
 };
+
+const SortedMonth = sortList(MonthTable, 'month');
+const SortedYear = sortList(YearTable, 'year');
+const SortedTable = sortList(SortTable, 'sort')
+
+function sortList (Component, type) {
+    function changeList (list) {
+        const sortedList = list.map(item => {
+            if (type === 'month') {
+                const month = new Date(item.date).toLocaleDateString('en-US', {month: 'short'});
+                return {month: month, amount: item.amount}
+            } else if (type === 'year') {
+                const year = new Date(item.date).getFullYear();
+                return {year: year, amount: item.amount}
+            } else { 
+                return item;
+            }
+        })
+        if (type === 'sort') {
+            sortedList.sort(compateDates);
+        }
+        return sortedList;
+    }
+
+    function compateDates(a,b) {
+        const first = new Date(a.date).getTime();
+        const second = new Date(b.date).getTime();
+        return first - second;
+    }
+
+    return class extends React.Component {
+        render() {
+            const sortedList = changeList(this.props.list);
+            return <Component {...this.props} list={sortedList} />
+        }
+    }
+}
