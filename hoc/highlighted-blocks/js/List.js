d63cc1041e5c678f2a8315d0df3e6315896be10e
Lev <agams92@gmail.com>
2018-08-07 13:43:43 +0300

    In which our hero wrapped articles and video

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -5,13 +5,26 @@ const List = props => {
         switch (item.type) {
             case 'video':
                 return (
-                    <Video {...item} />
+                    <WrappedVideo {...item} />
                 );
 
             case 'article':
                 return (
-                    <Article {...item} />
+                    <WrappedArticle {...item} />
                 );
         }
     });
 };
+
+const WrappedVideo = withWrap(Video);
+const WrappedArticle = withWrap(Article);
+
+function withWrap (Component) {
+    return class extends React.Component {
+        render() {
+            if (this.props.views < 100) return <New><Component {...this.props} /></New>
+            else if (this.props.views > 1000) return <Popular><Component {...this.props} /></Popular>
+            return <Component {...this.props} />
+        }
+    }
+}
